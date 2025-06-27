1d53b665dd9c146f405c4e34f9539e89bfff11ff
Alessandra Ulloa <233303@ids.upchiapas.edu.mx>
2025-06-27 17:17:16 -0600

    folder structure

---
+++ src/features/auth/page.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function AuthPage() {
+  return (
+    <div>
+      <h1>Authentication Page</h1>
+      <img src="/login.png" alt="Authentication" />
+      <p>This is where users can log in or sign up.</p>
+      {/* Add your authentication form or components here */}
+    </div>
+  );
+}
+
+export default AuthPage;
